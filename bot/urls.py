416f0eaca464b67fd4b2536e3cbd031299ecfbc4
Durgesh Barwal <durgeshbarwal@gmail.com>
2018-09-06 04:13:48 +0530

    Update urls.py

--- bot/urls.py
+++ bot/urls.py
@@ -4,6 +4,6 @@ from django.contrib import admin
 from .views import chat
 
 urlpatterns = [
-	url(r'^chat/$', chat, name='chat'),
+	url(r'^$', chat, name='chat'),
     url(r'^admin/', admin.site.urls)
 ]
